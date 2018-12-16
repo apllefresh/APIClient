43805479d2432957708567c0d3ed4723fdf92fea
apllefresh <arrogant93@mail.ru>
2018-12-16 22:47:06 +0300

    clear

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-import { Inventory } from './components/Inventory/Inventory';
-import { InventoryAct } from './components/Inventory/InventoryAct';
+import { Layout } from './Layout';
+
 
 export default class App extends Component {
   displayName = App.name
@@ -13,11 +9,9 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-            <Route path='/fetchdata' component={FetchData} />
-            <Route path='/inventory' component={Inventory} />
-            <Route path='/inventoryAct/:mode/:id?' component={InventoryAct} />
+        
+            
+            
       </Layout>
     );
   }
--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Col, Grid, Row } from 'react-bootstrap';
 import { NavMenu } from './components/NavMenu';
+import { Footer } from './components/Footer';
+import './Layout.css';
 
 export class Layout extends Component {
   displayName = Layout.name
@@ -12,9 +14,12 @@ export class Layout extends Component {
           <Col sm={1}>
             <NavMenu />
           </Col>
-          <Col sm={12}>
+          <Col id="body" sm={12}>
             {this.props.children}
           </Col>
+        </Row>
+        <Row>
+          <Footer/>
         </Row>
       </Grid>
     );
