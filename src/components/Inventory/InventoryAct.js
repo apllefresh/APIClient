14f4b1bcc2fede6dd239ea1b22c96c045023b76b
Konstantin Alekseev <kalexeev@gotrg.com>
2018-12-19 12:09:58 +0300

    create Inventory Act page

--- src/components/Inventory/InventoryAct.js
+++ src/components/Inventory/InventoryAct.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Col, Grid, Row, DropdownButton, MenuItem} from "react-bootstrap";
+import {Col, Grid, Row, DropdownButton, MenuItem, Button} from "react-bootstrap";
 import {InventoryBodyTable} from './InventoryBodyTable';
 import {InventoryDateDropDownList} from "./InventoryDateDropDownList";
 import {WarehouseDropDownList} from "./WarehouseDropDownList";
@@ -13,6 +13,7 @@ export class InventoryAct extends Component {
             mode: this.props.match.params.mode, id: this.props.match.params.id,
             selectInventoryId: 0,
             selectWarehouseId: [],
+            data:[]
         };
         this.updateWarehouseId = this.updateWarehouseId.bind(this);
     }
@@ -27,27 +28,52 @@ export class InventoryAct extends Component {
         console.log("warehouseID: " + value);
     };
 
+    componentDidMount() {
+        /*
+         fetch('api/inventoryDate/')
+             .then(response => response.json())
+             .then(data => {
+                 this.setState({ dateOptions: data, dateLoading: false });
+             });
+          */
+        var tempdata = [
+            {Id: 1,RowNumber:1,Ean:"4601231352829", Name : "Product 1",Count:10 },
+            {Id: 2,RowNumber:2,Ean:"4602204068413", Name : "Product 2",Count:10 },
+            {Id: 3,RowNumber:3,Ean:"4607654034893", Name : "Product 3",Count:10 },
+            {Id: 4,RowNumber:4,Ean:"4601231154658", Name : "Product 4",Count:10 },
+            {Id: 5,RowNumber:5,Ean:"4601231248063", Name : "Product 5",Count:10 },
+            {Id: 6,RowNumber:6,Ean:"4601235640463", Name : "Product 6",Count:10 },
+            {Id: 7,RowNumber:7,Ean:"4601234168404", Name : "Product 7",Count:10 },
+            {Id: 8,RowNumber:8,Ean:"4601232224505", Name : "Product 8",Count:10 }];
+        this.setState({ data: tempdata});
+    }
+    
     render() {
         return (
             <Grid fluid style={{height: '100%', overflow: 'auto'}}>
                 <h1>Inventory act </h1>
                 <Row style={{height: '100px'}}>
 
-                    <Col xs={6} md={4}>
+                    <Col md={4}>
                         <InventoryDateDropDownList updateData={this.updateInventoryDateId}/>
                     </Col>
-                    <Col xs={6} md={4}>
+                    <Col md={4}>
                         <WarehouseDropDownList updateDat={this.updateWarehouseId}/>
                     </Col>
-                    <Col xs={6} md={4}>
-                        <DropdownButton title="Dropdown" id="bg-vertical-dropdown-2">
-                            <MenuItem eventKey="1">Dropdown link</MenuItem>
-                            <MenuItem eventKey="2">Dropdown link</MenuItem>
+                    <Col md={1} >
+                        <DropdownButton title="Select Action" id="bg-vertical-dropdown-2">
+                            <MenuItem eventKey="1">Accept</MenuItem>
+                            <MenuItem eventKey="2">Revert</MenuItem>
                         </DropdownButton>
+                    </Col>
+                    <Col md={1} >
+                        <Button  bsStyle="primary">
+                            Export data from txt
+                        </Button>
                     </Col>
                 </Row>
                 <Row style={{height: '100%'}}>
-                    <InventoryBodyTable/>
+                    <InventoryBodyTable data={this.state.data}/>
                 </Row>
             </Grid>
 
