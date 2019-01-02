4ab310a6dee581103c7c25d2e343d877f23853ec
apllefresh <arrogant93@mail.ru>
2019-01-02 22:15:07 +0300

    navMenu and footer

--- src/NavMenu.js
+++ src/NavMenu.js
@@ -19,6 +19,15 @@ import {ReturnToShipperHead} from "./components/ManageBOL/ReturnToShipper/Return
 export class NavMenu extends Component {
     displayName = NavMenu.name;
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            inventoryShow: false,
+            ManageBOLShow: false,
+        };
+
+    }
+
     render() {
         return (
             <Navbar inverse fixedTop fluid collapseOnSelect>
@@ -30,68 +39,86 @@ export class NavMenu extends Component {
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav>
+                        <NavItem onClick={() => this.setState({inventoryShow: !this.state.inventoryShow})}>
+                            <Glyphicon glyph='education'/> Инвентаризация
+                        </NavItem>
+                        {
+                            this.state.inventoryShow ?
+                                <div>
+                                    <LinkContainer to={'/inventory'} Component={InventoryHead}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Акты инвентаризации
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/InventoryDates'} Component={InventoryDates}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Даты инвентаризации
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/warehouses'} Component={Warehouses}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Склады
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/remains'} Component={Remains}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Товарные остатки
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/InventoryResult'} Component={InventoryResult}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Результаты инвентаризации
+                                        </NavItem>
+                                    </LinkContainer>
+                                </div>
+                                : <p></p>
+                        }
+                        <NavItem onClick={() => this.setState({ManageBOLShow: !this.state.ManageBOLShow})}>
+                            <Glyphicon glyph='education'/> Накладные
+                        </NavItem>
+                        {
+                            this.state.ManageBOLShow ?
+                                <div>
+                                    <LinkContainer to={'/resortHead'} Component={ReSortHead}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Пересорт
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/rePriceHead'} Component={RePriceHead}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Переоценка
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/movingHead'} Component={MovingHead}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Перемещения
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/RequestHead'} Component={RequestHead}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Заявки
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/InvoiceHead'} Component={InvoiceHead}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Накладные
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/AnnulmentHead'} Component={AnnulmentHead}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Списание
+                                        </NavItem>
+                                    </LinkContainer>
+                                    <LinkContainer to={'/ReturnToShipperHead'} Component={ReturnToShipperHead}>
+                                        <NavItem>
+                                            <Glyphicon glyph='education'/> Возврат поставщику
+                                        </NavItem>
+                                    </LinkContainer>
+                                </div>
+                                : <p></p>
+                        }
 
-                        <LinkContainer to={'/inventory'} Component={InventoryHead}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Инвентаризаия
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/InventoryDates'} Component={InventoryDates}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Даты инвентаризации
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/warehouses'} Component={Warehouses}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Склады
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/remains'} Component={Remains}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Товарные остатки
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/InventoryResult'} Component={InventoryResult}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Результаты инвентаризации
-                            </NavItem>
-                        </LinkContainer>
-
-                        <LinkContainer to={'/resortHead'} Component={ReSortHead}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Пересорт
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/rePriceHead'} Component={RePriceHead}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Переоценка
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/movingHead'} Component={MovingHead}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Перемещения
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/RequestHead'} Component={RequestHead}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Заявки
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/InvoiceHead'} Component={InvoiceHead}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Накладные
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/AnnulmentHead'} Component={AnnulmentHead}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Списание
-                            </NavItem>
-                        </LinkContainer>
-                        <LinkContainer to={'/ReturnToShipperHead'} Component={ReturnToShipperHead}>
-                            <NavItem>
-                                <Glyphicon glyph='education'/> Возврат поставщику
-                            </NavItem>
-                        </LinkContainer>
+                       
 
 
                     </Nav>
