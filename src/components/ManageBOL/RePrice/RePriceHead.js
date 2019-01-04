9c3b9c04f1f5457c00b8be0b8ff62e546904a8ca
apllefresh <arrogant93@mail.ru>
2019-01-04 09:55:09 +0300

    fixed DateTimePicker

--- src/components/ManageBOL/RePrice/RePriceHead.js
+++ src/components/ManageBOL/RePrice/RePriceHead.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Grid, Row, Col, FormGroup, ControlLabel, Panel} from 'react-bootstrap';
 import {WarehouseDropDownList} from "../../Inventory/WarehouseDropDownList";
 import {RePriceHeadTable} from "./RePriceHeadTable";
-import DatePicker from "react-date-picker";
+import DatePicker from "react-datepicker";
 
 export class RePriceHead extends Component {
 
